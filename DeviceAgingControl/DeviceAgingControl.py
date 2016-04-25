358903a722b524db15b58896e40caa9c1dfdc1b9
Camilo <cadiazb@berkeley.edu>
2016-04-25 12:32:45 -0700

    Initial attempt to creating peristaltic pump class

--- DeviceAgingControl/DeviceAgingControl.py
+++ DeviceAgingControl/DeviceAgingControl.py
@@ -6,7 +6,14 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import sys
 
-class HellowWorldGTK:
+class PeristalticPump:
+  
+    def _initt__(self):
+	self.FlowRate = 0
+	self.Period = 1
+	self.TimeON = 0.5
+  
+class AgingSystemControl:
 
     def __init__(self):
         self.gladefile = "DeviceAgingControl.glade" 
@@ -15,10 +22,13 @@ class HellowWorldGTK:
         self.glade.connect_signals(self)
         self.glade.get_object("ControlPanel").show_all()
 	self.glade.get_object("ControlPanel").connect("delete-event", Gtk.main_quit)
+	
+	#Create Peristaltic pump
+	pPump = PeristalticPump()
 
 if __name__ == "__main__":
     try:
-        win = HellowWorldGTK()
+        win = AgingSystemControl()
 	Gtk.main()
     except KeyboardInterrupt:
         pass
